3ac45766e7edc3507d4bf4c04098d55acaf32e9f
Dr. Tristan Behrens <tristan@ai-guru.de>
2024-09-04 08:25:41 +0200

    Adds tests for HF models.

---
+++ tests/test_hf_models.py
@@ -0,0 +1,40 @@
+# Add the parent directory to the path to be able to import the languagemodel module
+import os
+script_folder = os.path.dirname(os.path.realpath(__file__))
+import sys
+sys.path.append(os.path.join(script_folder, ".."))
+
+# Regular imports.
+from source.languagemodel import LanguageModel
+
+# Define the model ids.
+model_ids = [
+    "TristanBehrens/bach-garland-xlstm",
+    "TristanBehrens/bach-garland-mamba",
+    "TristanBehrens/bach-garland-pharia",
+    "TristanBehrens/bach-garland-transformer",
+]
+
+# Test all models.
+successful_models = []
+for model_id in model_ids:
+    print(f"Testing model: {model_id}...")
+    try:
+        model = LanguageModel(model_id)
+        model.summary()
+        output_dict = model.generate(
+            prompt="GARLAND_START",
+            temperature=0.5,
+            max_length=128,
+            end_tokens=[],
+            forbidden_tokens=[],
+            return_structured_output=True
+        )
+        print(output_dict)
+        successful_models.append(model_id)
+    except Exception as e:
+        print(f"Error: {e} for model_id: {model_id}")
+        continue
+
+# Print the successful models.
+print(f"Successful models: {successful_models}")
