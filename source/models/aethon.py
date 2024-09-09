36803e9cd990545f54b751a6e000d8c23af2e29c
Dr. Tristan Behrens <tristan@ai-guru.de>
2024-09-09 14:56:42 +0200

    Adds aethon training. Adds copyright notices.

--- source/models/aethon.py
+++ source/models/aethon.py
@@ -1,3 +1,19 @@
+# Helibrunna - A HuggingFace compatible xLSTM trainer.
+# Copyright (c) 2024 Dr. Tristan Behrens
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+ 
 from dataclasses import dataclass
 import torch
 import torch.nn as nn
@@ -70,6 +86,7 @@ class AethonLayer(nn.Module):
 
 
 class Aethon(nn.Module):
+
     def __init__(self, config: AethonConfig):
         super(Aethon, self).__init__()
         self.embedding = nn.Embedding(config.vocab_size, config.dim)
